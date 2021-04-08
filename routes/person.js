57118fa31f1c350aa0c449057a7b02a8c5e1f040
Ekta <dudh0004@algonquinlive.com>
2021-04-08 00:58:35 -0400

    POST route of person

--- routes/person.js
+++ routes/person.js
@@ -8,8 +8,27 @@ const debug = createDebug('giftr:routes:people')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-    const collection = await Person.find()
-    res.send({ data: collection })
+    const person = await Person.find()
+    res.send({ data: person })
+})
+
+router.post('/', sanitizeBody, async (req, res) => {
+    let newPerson = new Person(req.sanitizedBody)
+    try {
+        await newPerson.save()
+        res.status(201).send({ data: newPerson })
+    } catch (err) {
+        debug(err)
+        res.status(500).send({
+        errors: [
+        {
+            status: '500',
+            title: 'Server error',
+            description: 'Problem saving document to the database.',
+        },
+        ],
+    })
+    }
 })
 
 export default router;
